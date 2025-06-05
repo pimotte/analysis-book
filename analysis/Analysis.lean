ada446d314d93204dc3efdf54c41fe48d9853e61
Pim Otte <p.j.otte@uu.nl>
2025-06-05 22:28:31 +0200

    Update Analysis.lean

--- analysis/Analysis.lean
+++ analysis/Analysis.lean
@@ -4,7 +4,9 @@ import Analysis.Section_2_3
 import Analysis.Section_2_epilogue
 import Analysis.Section_3_1
 import Analysis.Section_4_1
-import Analysis.Section_4_1
-import Analysis.Section_4_1
+import Analysis.Section_4_2
+import Analysis.Section_4_3
 import Analysis.Section_5_1
 import Analysis.Section_5_2
+import Analysis.Section_5_3
+import Analysis.Section_5_4
